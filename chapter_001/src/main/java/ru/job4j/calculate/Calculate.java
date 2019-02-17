0b27bce31c87639f6c4a441d62eea9814cbfd3ca
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-02-17 22:08:04 +0700

    1.5. Checkstyle[#114143]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,12 +7,12 @@ package ru.job4j.calculate;
 * @version 1
 */
 
-public class Calculate{
+public class Calculate {
 /**
 * Method main
 * @param args - args.
 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 /**
