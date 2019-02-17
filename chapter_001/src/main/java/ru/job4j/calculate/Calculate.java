5ef4e0fc4eb690f03bb054a08ad7815e59d5b199
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-02-17 16:56:37 +0700

    1.3.1 Maven Plugins[#114138]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,9 +8,8 @@ package ru.job4j.calculate;
 */
 
 public class Calculate{
-\**
+/**
 * Method main
-* @return "Hello World"
 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
