d93e1f102a6707b0dc22f8154ccf48d46594f260
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-02-19 14:58:45 +0700

    3.2. Конвертер валюты.[#114129]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/ConverterTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+    @Test
+    public void when66RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(66);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when75RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(75);
+        assertThat(result, is(1));
+    }
+}
