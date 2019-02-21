414003a77d81e1891cd31570b92dd337c948cc2d
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-02-21 12:09:23 +0700

    5.5. Рефакторинг кода.[#114110]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,77 +1,50 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
 * Paint  piramid.
 * @author Vasiliy Gasevskiy
 * @since 20.02.2019
 * @version 1
 */
-
 public class Paint {
-/**
-* Method piramid.
-* @param h высота пирамиды.
-* @return piramid.
-*/
-	 public String piramid(int h) {
-		String ln = System.lineSeparator();
-		StringBuilder result = new StringBuilder();
-		for (int j = 0; j != h; j++) {
-			for (int i = 0; i != (h * 2) - 1; i++) {
-				if (j >= Math.abs(i - (h - 1))) {
-					result.append("^");
-				} else {
-					result.append(" ");
-				}
-			}
-			result.append(ln);
-		}
-		return result.toString();
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
-	
-	
-/**
-* Method rightTrl.
-* @param h высота пирамиды.
-* @return rightTrl.
-*/
-	public String rightTrl(int height) {
-        // Буфер для результата.
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте. 
-        int width = height; 
-        // внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != width; column++) {
-                // если строка равна ячейке, то рисуем галку. 
-                // в данном случае строка определяет, сколько галок будет в строке
-                if (row >= column) {
+            for (int column = 0; column != widht; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
                 }
             }
-            // добавляем перевод строки.
             screen.append(System.lineSeparator());
         }
-        // Получаем результат.
         return screen.toString();
     }
-	
-	public String leftTrl(int height) {
-    StringBuilder screen = new StringBuilder();
-    int width = height;
-    for (int row = 0; row != height; row++) {
-        for (int column = 0; column != width; column++) {
-            if (row >= width - column - 1) {
-                screen.append("^");
-            } else {
-                screen.append(" ");
-            }
-        }
-        screen.append(System.lineSeparator());
-    }
-    return screen.toString();
-}
 }
