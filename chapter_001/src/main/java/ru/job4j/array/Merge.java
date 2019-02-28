81563bf8890755fa6c6264d300f396b08a92d4a8
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-02-28 12:00:42 +0700

    Экзамен. Базовый синтаксис.[#114148]

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,23 +1,33 @@
 package ru.job4j.array;
 
+/**
+ * Merge two increaseing arrays into the one.
+ *
+ * @author Vasiliy Gasevskiy
+ * @version 2
+ */
+
 public class Merge {
-    public static void main(String[] args) {
-        int[] a = new int[]{1,3,5,67,122,889};
-        int[] b = new int[]{-10, -8 ,-2, 0, 2, 23, 44, 221};
-        int[]c = add(a, b);
-        for (int x:c){
-            System.out.println(x);
-        }
-    }
+
+    /**
+     * Method main
+     *
+     * @param a - first array.
+     * @param b - second array
+     * @return c - result array
+     */
 
     public static int[] add(int[] a, int[] b) {
-    int aL = a.length;
-    int bL = b.length;
-    int[] c = new int[aL + bL];
-    int i = 0, j = 0;
-    while (i < aL || j < bL)
-        c[i + j] = (i < aL && (j == bL || a[i] < b[j])) ? a[i++] : b[j++];
-
-    return c;
+        if (a == null || b == null) {
+            return (a == null) ? b : a;
+        }
+        int aL = a.length;
+        int bL = b.length;
+        int[] c = new int[aL + bL];
+        int i = 0, j = 0;
+        while (i < aL || j < bL) {
+            c[i + j] = (i < aL && (j == bL || a[i] < b[j])) ? a[i++] : b[j++];
+        }
+        return c;
     }
 }
