52172fde9aa98969fb2ecaa5a8de4a8a3cd23d0a
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-03-11 11:51:01 +0700

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#114180]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,17 +14,18 @@ import static org.junit.Assert.assertThat;
  */
 
 public class StartUITest {
-    Tracker tracker = new Tracker();
-    Item item = tracker.add(new Item("test name", "desc"));
-    Item item2 = tracker.add(new Item("test name 2", "desc2"));
-    Item item3 = tracker.add(new Item("test name 3", "desc3"));
-    String id = item.getId();
+
 
     /**
      * Тест добавления нового элемнета.
      */
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name 2", "desc2"));
+        Item item3 = tracker.add(new Item("test name 3", "desc3"));
+        String id = item.getId();
         Input input = new StubInput(new String[]{"0", "test name", "desc", "7"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[3].getTask(), is("test name"));
@@ -36,6 +37,11 @@ public class StartUITest {
      */
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name 2", "desc2"));
+        Item item3 = tracker.add(new Item("test name 3", "desc3"));
+        String id = item.getId();
         Input input = new StubInput(new String[]{"2", id, "test replace", "заменили заявку", "7"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findById(id).getTask(), is("test replace"));
@@ -46,6 +52,11 @@ public class StartUITest {
      */
     @Test
     public void whenDeleteThenTrackerHasNoValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name 2", "desc2"));
+        Item item3 = tracker.add(new Item("test name 3", "desc3"));
+        String id = item.getId();
         Input input = new StubInput(new String[]{"3", id, "7"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll().length, is(2));
@@ -56,6 +67,11 @@ public class StartUITest {
      */
     @Test
     public void whenFindByIdThenTrackerHasItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name 2", "desc2"));
+        Item item3 = tracker.add(new Item("test name 3", "desc3"));
+        String id = item.getId();
         Input input = new StubInput(new String[]{"6", id, "comment", "7"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findById(id).getComments(), is("comment"));
