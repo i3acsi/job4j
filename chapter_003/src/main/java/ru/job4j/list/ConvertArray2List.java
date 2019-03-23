cf3a286a3cd455311ec36ff4eae0eb3819bc3187
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-03-23 22:41:29 +0700

    1. Конвертация листа массивов в один лист Integer[#114170]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertArray2List.java
@@ -0,0 +1,11 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConvertArray2List {
+    public List<Integer> convert (List<int[]> list){
+        return list.stream().flatMapToInt(Arrays::stream).boxed().collect(Collectors.toList());
+    }
+}
--- chapter_003/src/test/java/ru/job4j/list/ConvertArray2ListTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertArray2ListTest.java
@@ -1,4 +1,23 @@
 package ru.job4j.list;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class ConvertArray2ListTest {
+    @Test
+    public void when2ElementsAnd4Elements() {
+        ConvertArray2List array = new ConvertArray2List();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new  int[]{3, 4, 5, 6});
+        List<Integer> result = array.convert(list);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(result, is(expected));
+    }
 }
