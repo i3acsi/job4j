3a8676a2435d072c5c80ba522b01ba5623233e76
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-03-25 15:57:31 +0700

    2. Сортировка User с использованием Comparator[#114157]

--- chapter_003/src/main/java/ru/job4j/list/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/list/SortUser.java
@@ -4,11 +4,15 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class SortUser {
-    public Set<User> sort (List<User> users){
+    public Set<User> sort(List<User> users) {
         return new TreeSet<>(users);
     }
 
-    public List<User> sortNameLength (List<User> users){
+    public List<User> sortNameLength(List<User> users) {
         return users.stream().sorted(Comparator.comparingInt(x -> x.getName().length())).collect(Collectors.toList());
+    }
+
+    public List<User> sortByAllFields(List<User> users) {
+        return users.stream().sorted().sorted(Comparator.comparing(User::getName)).collect(Collectors.toList());
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
@@ -13,7 +13,8 @@ public class SortUserTest {
             new User(0, "Vasiliy", "Gasevskiy", "Nsk", 32),
             new User(2, "Petr", "Arsentev", "Bryansk", 31),
             new User(2, "Ivan", "Ivanov", "Bryansk", 34),
-            new User(3, "Child", "Kid", "Kinder Garden", 4)
+            new User(3, "Child1", "Kid", "Kinder Garden", 4)
+            new User(3, "Child2", "Kid", "Kinder Garden", 5)
     };
 
     @Test
@@ -36,5 +37,20 @@ public class SortUserTest {
         expected.add(users[3]);
         expected.add(users[0]);
         assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortedTwice() {
+        List<User> result  = sortUser.sortByAllFields(Arrays.asList(users));
+        List<User> expected = new ArrayList<>();
+        expected.add(users[3]);
+        expected.add(users[2]);
+        expected.add(users[1]);
+        expected.add(users[0]);
+        for (User u: result) {
+            System.out.println(u.getName());
+        }
+        assertThat(result, is(expected));
+
     }
 }
