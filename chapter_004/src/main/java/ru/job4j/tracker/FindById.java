f2911717b5d035f30ef77af741be531369ef7756
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-04-12 14:51:16 +0700

    2. В классе StartUI заменить вывод в консоль на Consumer.[#114062]

---
+++ chapter_004/src/main/java/ru/job4j/tracker/FindById.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker;
+
+/**
+ * Класс для поиска заявки по ID.
+ */
+public class FindById extends  BaseAction {
+    
+    public FindById(int key, String info) {
+        super(key, info);
+    }
+	
+	@Override
+	public void execute(Input input, Tracker tracker) {
+	    System.out.println("----------Поиск заявки по ID----------");
+        String id = input.ask("Введите id искомой заявки ");
+        Item temp = tracker.findById(id);
+        if (temp != null) {
+            System.out.println("Найдена заявка " + temp.show());
+        } else {
+            System.out.println("---------Заявка не найдена---------");
+        }
+	}
+}
