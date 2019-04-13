26ac9c11b0afddbc7288e72dae50a7f889c4d59f
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-04-13 13:04:07 +0700

    4. Преобразование List в Map.[#114072]

--- chapter_004/src/test/java/ru/job4j/filter/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/filter/SchoolTest.java
@@ -1,12 +1,30 @@
 package ru.job4j.filter;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class SchoolTest {
-    private static School school = new School(100);
-    private static List<Student> students = school.getStudents();
+    private static School school;
+    private static List<Student> students;
+
+    @Before
+    public void before() {
+        school = new School(100);
+        students = school.getStudents();
+    }
+
+    @After
+    public void after() {
+        students.clear();
+    }
 
     @Test
     public void from70to100() {
@@ -14,7 +32,7 @@ public class SchoolTest {
         System.out.println("from 70 to 100");
         for (Student s : result) {
             int score = s.getScore();
-            System.out.println(score);
+            System.out.println(s.toString());
             assert (score > 70 & score <= 100);
         }
     }
@@ -25,7 +43,7 @@ public class SchoolTest {
         System.out.println("from 50 to 70");
         for (Student s : result) {
             int score = s.getScore();
-            System.out.println(score);
+            System.out.println(s.toString());
             assert (score > 50 & score <= 70);
         }
     }
@@ -36,8 +54,17 @@ public class SchoolTest {
         System.out.println("from 0 to 50");
         for (Student s : result) {
             int score = s.getScore();
-            System.out.println(score);
+            System.out.println(s.toString());
             assert (score >= 0 & score <= 100);
         }
+    }
+
+    @Test
+    public void collectToMapTest() {
+        Map<String, Student> result = school.collectToMap(students);
+        List<Student> res = new ArrayList<>(result.values());
+        res.sort(Student::compareTo);
+        students.sort(Student::compareTo);
+        assertThat(res, is(students));
     }
 }
