bc9af21fb087615b2bad3a80cf621416f94ac864
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-04-19 16:01:36 +0700

    5.1.1. Итератор для двухмерного массива int[][][#134863]

--- chapter_005/src/main/java/ru/job4j/services/JaggedArrayIterator.java
+++ chapter_005/src/main/java/ru/job4j/services/JaggedArrayIterator.java
@@ -2,53 +2,30 @@ package ru.job4j.services;
 
 import java.util.Iterator;
 
+
 public class JaggedArrayIterator implements Iterator {
     private final int[][] array;
-    private int counter = 0;
-    private IteratorArray ir;
-
+    private int cols = 0, rows = 0;
 
     public JaggedArrayIterator(int[][] array) {
         this.array = array;
-        this.ir = new IteratorArray(array[0]);
     }
 
     @Override
     public boolean hasNext() {
-        boolean result = ir.hasNext();
-        if (!result & counter < array.length - 1) {
-            IteratorArray tempIr = new IteratorArray(array[counter + 1]);
-            result = tempIr.hasNext();
-        }
-        return result;
+        return (rows < array.length && cols < array[rows].length);
     }
 
     @Override
     public Object next() {
-        if (!ir.hasNext()) {
-            counter++;
-            ir = new IteratorArray(array[counter]);
+        Object result = array[rows][cols];
+        if (cols < array[rows].length - 1) {
+            cols++;
+        } else {
+            rows++;
+            cols = 0;
         }
-        return ir.next();
+        return result;
     }
 }
-
-//        // System.out.println(array[0].length + " " + array[1].length + array.length);
-//        int temp = 0;
-//        int result = 0;
-//        point:
-//        for (int i = 0; i < array.length; i++) {
-//            for (int j = 0; j < array[i].length; j++) {
-//                //System.out.println(array[i][j]);
-//                if (temp == counter) {
-//                    counter++;
-//                    result =  array[i][j];
-//                    break point;
-//                } else {
-//                    temp++;
-//                }
-//            }
-//        }
-//
-//        return result;
 
