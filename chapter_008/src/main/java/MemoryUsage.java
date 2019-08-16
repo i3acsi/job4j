035c1fd14065e150764f01d69a3e3ba0044364c2
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2019-08-16 12:49:49 +0700

    gc1

--- chapter_008/src/main/java/MemoryUsage.java
+++ chapter_008/src/main/java/MemoryUsage.java
@@ -3,21 +3,19 @@ public class MemoryUsage {
     private static final Runtime runtime = Runtime.getRuntime();
 
     public static void main(String[] args) {
-
-        System.out.println(runtime.totalMemory() - runtime.freeMemory());
         System.gc();
-        //String name ="name";
-        //String[] u = new String[1000_000];
+        long before, after;
+        String name ="name";
+        String[] u = new String[1000_000];
         //User[] u = new User[1000_000];
         //Object[] u = new Object[1000_000];
-        EmptyUser[] u = new EmptyUser[1000_000];
-        System.out.println(runtime.totalMemory() - runtime.freeMemory());
-        System.out.println(runtime.totalMemory() - runtime.freeMemory());
+        //EmptyUser[] u = new EmptyUser[1000_000];
+        before = (runtime.totalMemory() - runtime.freeMemory());
         for (int i = 0; i < 1000_000; i++) {//EmptyUser u: users) {
             u[i]
-                    //=new User(name);//48 байт
-                    = new EmptyUser();//40 байт
-                    //=String.valueOf(i);//48 байт
+                    //=new User(name);//16 байт
+                    //= new EmptyUser();//8 байт
+                    =name+i;//48 байт
                     //= new Object();// 8 байт
 //            {
 //                @SuppressWarnings("deprecation")
@@ -29,10 +27,9 @@ public class MemoryUsage {
 //
 //            };
         }
-
-        System.out.println(runtime.totalMemory() - runtime.freeMemory());
+        after = (runtime.totalMemory() - runtime.freeMemory());
         System.gc();
-        System.out.println(runtime.totalMemory() - runtime.freeMemory());
+        System.out.println((after-before)/1_000_000);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -48,20 +45,20 @@ public class MemoryUsage {
             this.name = name;
         }
 
-        @SuppressWarnings("deprecation")
-        @Override
-        protected void finalize() throws Throwable {
-            //System.out.print(this);
-            System.out.println("  finalize");
-            super.finalize();
-        }
-
-        @Override
-        public String toString() {
-            return "User{" +
-                    "name='" + name + '\'' + this +
-                    '}';
-        }
+//        @SuppressWarnings("deprecation")
+//        @Override
+//        protected void finalize() throws Throwable {
+//            //System.out.print(this);
+//            System.out.println("  finalize");
+//            super.finalize();
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "User{" +
+//                    "name='" + name + '\'' + this +
+//                    '}';
+//        }
     }
 
     public static class EmptyUser {
@@ -69,18 +66,18 @@ public class MemoryUsage {
         public EmptyUser() {
         }
 
-        @SuppressWarnings("deprecation")
-        @Override
-        protected void finalize() throws Throwable {
-            //System.out.println(this);
-            super.finalize();
-            System.out.println("finalize");
-        }
-
-        @Override
-        public String toString() {
-            return "EmptyUser{}" + this;
-        }
+//        @SuppressWarnings("deprecation")
+//        @Override
+//        protected void finalize() throws Throwable {
+//            //System.out.println(this);
+//            super.finalize();
+//            System.out.println("finalize");
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "EmptyUser{}" + this;
+//        }
     }
 
 }
